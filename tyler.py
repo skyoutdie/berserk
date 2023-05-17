7e9a62d5d0339f26de45f686d019b806f7e08795
skyoutdie <133123275+skyoutdie@users.noreply.github.com>
2023-05-17 06:53:39 -0600

    Create tyler.py

---
+++ tyler.py
@@ -0,0 +1,27 @@
+nombre = "Tyler, The Creator"
+descripcion_carrera = """
+Tyler Gregory Okonma, conocido profesionalmente como Tyler, The Creator, es un rapero, productor musical, director de videos y diseñador de moda estadounidense. Nació el 6 de marzo de 1991 en Los Ángeles, California. Comenzó su carrera musical en 2007 como miembro fundador del colectivo de hip-hop Odd Future. Su estilo musical es ecléctico, fusionando elementos del rap, R&B, soul y jazz.
+
+A lo largo de su carrera, Tyler, The Creator ha lanzado varios álbumes aclamados por la crítica, incluyendo 'Goblin', 'Wolf', 'Cherry Bomb', 'Flower Boy' e 'Igor'. Su música ha sido reconocida por su originalidad y letras introspectivas. En 2020, ganó un premio Grammy al Mejor Álbum de Rap por 'Igor'.
+
+Además de su carrera musical, Tyler, The Creator es conocido por su trabajo en el mundo de la moda. Lanzó su propia línea de ropa llamada Golf Wang y ha colaborado con marcas como Converse. También ha dirigido videos musicales y ha incursionado en la producción de programas de televisión y películas animadas.
+"""
+
+familia = """
+Tyler, The Creator es bastante reservado con respecto a su vida personal y no ha revelado muchos detalles sobre su familia. Sin embargo, ha mencionado en entrevistas que tiene una buena relación con su madre y su abuela. A menudo les agradece por su apoyo en su música.
+
+Además, Tyler, The Creator tiene un estrecho vínculo con su familia de Odd Future, el colectivo musical con el que comenzó su carrera. Los miembros de Odd Future se consideran una especie de familia extendida y han colaborado en numerosos proyectos juntos a lo largo de los años.
+"""
+
+aficiones = """
+Como persona, Tyler, The Creator tiene una amplia variedad de intereses y aficiones. Es conocido por su amor por la moda y el diseño, lo que se refleja en su propia línea de ropa, Golf Wang. También es un apasionado del arte y ha trabajado en proyectos visuales relacionados con su música.
+
+Además, Tyler, The Creator disfruta de la cinematografía y ha expresado su interés en dirigir más películas en el futuro. También es un fanático del skateboarding y ha sido visto en varias ocasiones practicando este deporte.
+
+En su tiempo libre, a Tyler, The Creator le gusta pasar tiempo con sus amigos y colaboradores musicales. También es activo en las redes sociales, donde comparte su creatividad y se conecta con sus seguidores.
+"""
+
+# Imágenes (debes reemplazar los enlaces por las URL de las imágenes correspondientes)
+imagen_carrera = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vulture.com%2F2020%2F01%2Fgrammys-2020-tyler-the-creator-criticizes-rap-album-win.html&psig=AOvVaw3PDizBbPBh2VnbyYSgRujX&ust=1684414077324000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCPCp3tnM_P4CFQAAAAAdAAAAABAD"
+imagen_familia = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.romper.com%2Fp%2Ftyler-the-creators-mom-went-on-stage-at-the-grammys-bawled-21748377&psig=AOvVaw3goL9V6Q02YQvsIgE6D5is&ust=1684414149399000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCJCU9fPM_P4CFQAAAAAdAAAAABAJ"
+imagen_aficiones = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.trendhunter.com%2Ftrends%2Fgolf-wang-fall-winter&psig=AOvVaw1gulWDYqh2u9YR7jir7K-g&ust=1684414211143000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCNDW7ZHN_P4CFQAAAAAdAAAAABAI"
