55cb809150cb4d368e6841ed3e96290dc67c7f3e
skyoutdie <133123275+skyoutdie@users.noreply.github.com>
2023-05-17 06:34:39 -0600

    Create Sum_Python.py

---
+++ Sum_Python.py
@@ -0,0 +1,13 @@
+nombre = "Tyler, The Creator"
+descripcion = "Tyler Gregory Okonma, conocido profesionalmente como Tyler, The Creator, es un rapero, productor musical, director de videos y diseñador de moda estadounidense."
+carrera = "Tyler, The Creator comenzó su carrera musical en 2007 como miembro fundador del colectivo de hip-hop Odd Future. En 2011, lanzó su primer álbum de estudio, Goblin, que debutó en el número cinco en el Billboard 200 y fue certificado oro por la RIAA. Desde entonces, ha lanzado varios álbumes aclamados por la crítica, incluyendo Wolf, Cherry Bomb, Flower Boy y Igor."
+proyectos = "Además de su carrera musical, Tyler, The Creator también ha incursionado en la moda, lanzando su propia línea de ropa llamada Golf Wang. También ha dirigido videos musicales y ha creado programas de televisión y películas animadas."
+premios = "Tyler, The Creator ha recibido varios premios a lo largo de su carrera, incluyendo un Grammy al Mejor Álbum de Rap por Igor en 2020. También ha ganado varios MTV Video Music Awards y BET Hip Hop Awards."
+exitos = "Algunos de los mayores éxitos de Tyler, The Creator incluyen canciones como 'Yonkers', 'Earfquake', 'See You Again' y 'Who Dat Boy'."
+
+print(nombre)
+print(descripcion)
+print(carrera)
+print(proyectos)
+print(premios)
+print(exitos)
